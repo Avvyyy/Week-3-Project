2ac11b409c560d936cb8ae44bcfa9b2531f1d025
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-04-13 22:44:50 -0700

    first commit

---
+++ encryption.php
@@ -0,0 +1,11 @@
+<?php
+    $name = "My name is Favour A. Uzochukwu";
+
+    $encrypted = md5($name);
+
+    echo ("The encrypted vaalue of $name is: $encrypted");
+?>
+
+/*Explanation of the FIzzbuzz Project:
+    The Fizzbuzz project loops through numbers 1 to 100, prints Fizzbuzz if the number is divisible by 3 nd 5, Fizz if divisible by 3 and Buzz if divisible by 5
+*/
